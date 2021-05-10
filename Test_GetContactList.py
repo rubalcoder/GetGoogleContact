199f445cf55d7db34dfc140f29895de1b5d8a95d
rubalcoder <66204470+rubalcoder@users.noreply.github.com>
2021-05-10 11:30:53 +0530

    Add files via upload

--- Test_GetContactList.py
+++ Test_GetContactList.py
@@ -1,19 +1,33 @@
+# Python Module to download Google Contacts on local system and verify and validate the contacts
+# Python Request Library used to execute CRUD operations with HTTP RestAPi published by Google
+# Json Library used to serialize/deserialize response data into JSON format
+# Code Written in Python 3.7
+# Pytest Framework used to collect and execute test cases in the module
+# Start with importing the external modules and libraries into the main module
+#
+#
 import os
 import json
 import requests
 import pytest
 
-
+# Declare the configuration parameters at the start of the script
+# PatsToken or personal access token is generated to authorize the http communication over RestAPi, follow readme for more info
 BaseUrl = 'https://people.googleapis.com/v1/people/me/connections'
-Parameters = ['phoneNumbers', 'names', 'emailAddress']
+PatsToken = 'ya29.a0AfH6SMAqFhch7jfCGtO7cU_Hvi7TjoB9tqI4sGh7Iy2AL5lpXPJn8DxPWKUGrbZOxQ7oh74c4I3fYn8FQDRrtH2eSEtq69GR19shSH4XnqGuisV8Sq2u27m7LCS_AUYrsKy3QAQJGGcZXREGoywQhkjfNhZk0g'
 payload = {}
-headers = {
-  'Authorization': 'Bearer ya29.a0AfH6SMCjkrDurR-z0yruJ5xrzZ8XxYP4-z2mZZcKOumT2L-hdfG6_S40y0-eZRAkGKrRTwtxScoSbAq9fuP4M0_J7G3Lw-Ekilpz_ciIHQ_uQwXkgyQ94t1oSzVNmbW1cnQf_pKpSgzhLaYasMerlImfR66Q'
-}
+headers = {'Authorization': 'Bearer '+PatsToken}
+
+# Declare the Test data to parameterize test cases and perform data driven test sets
+Parameters = ['phoneNumbers', 'names', 'emailAddress']
+Parameters_Name = ["Dilip Gutthi", 'ross taylor', 'Chaudhary Uncle']
+Parameters_PhoneNumber = ['012345', '23456', '78902']
+Parameters_EmailAddress = ['a@sample.com', 'b@sample.com', 'c@sample.com']
 
 
 @pytest.fixture
 def getContactList():
+    '''Function declared as a fixture to get the contact list with a mask parameter as name  '''
     url = BaseUrl+"?personFields=names"
     response = requests.request("GET", url, headers=headers, data=payload)
     jsonResponse = json.loads(response.text)
@@ -22,6 +36,7 @@ def getContactList():
 
 @pytest.fixture
 def getDataFromJson():
+    '''Function declared as a fixture to get the contact list information from JSON file'''
     file1 = open("ContactList.json", "r")
     rJson = json.load(file1)
     file1.close()
@@ -30,7 +45,11 @@ def getDataFromJson():
 
 @pytest.mark.usefixtures("getContactList")
 class Test_ValidateContactList:
+    '''Pytest test Class created with different test methods wrapped inside he class to download and validate the
+    contactList with test cases associated to fixture, some of the test cases marked as smoke.
+    Assert statements available to validate the tests'''
 
+    @pytest.mark.smoke
     def test_validateResponse(self, getContactList):
         assert getContactList[0] == 200 , "request not successfull"
 
@@ -43,13 +62,15 @@ class Test_ValidateContactList:
         fileObj.close()
         assert os.path.isfile("ContactList.json") , "File did not create successfully"
 
-    def test_verifyTestCaseCountMatch(self, getContactList, getDataFromJson):
+    @pytest.mark.smoke
+    def test_verifyContactListCountMatch(self, getContactList, getDataFromJson):
         assert getContactList[1] == getDataFromJson , "Data Mismatch"
 
     def test_verifyDataMatchFileandtext(self, getContactList, getDataFromJson):
         assert len(getContactList[1]['connections'][0]['resourceName']) == len(getDataFromJson['connections'][0]['resourceName']), "Data Mismatch"
 
 
+# Pytest fixture to fetch data according to the personFields mask
 @pytest.fixture()
 def getContactListWithParameters():
     url = BaseUrl+"?personFields=phoneNumbers,names,emailAddresses"
@@ -60,10 +81,13 @@ def getContactListWithParameters():
 
 @pytest.mark.usefixtures("getContactListWithParameters")
 class Test_ValidateDataContactList:
+    '''pytest test class with test cases wrapped and paramterized to validate the data against the data set parameters'''
 
 
-    @pytest.mark.parametrize('name', ["Dilip Gutthi", 'ross taylor', 'Chaudhary Uncle'])
+    @pytest.mark.parametrize('name', Parameters_Name)
     def test_verifyNamesInJsonResponse(self, getContactListWithParameters, name):
+        '''Pytest test case to validate the contact names inside the contactList. Test case execute multiple times
+        according to the parameterized data '''
         count = 0
         for i in range (0, len(getContactListWithParameters['connections'])):
 
@@ -74,6 +98,56 @@ class Test_ValidateDataContactList:
             assert True
         else:
             assert False , name + " does not exist in the contact list"
+
+    @pytest.mark.smoke
+    @pytest.mark.parametrize('phoneNumber', Parameters_PhoneNumber)
+    def test_verifyNamesInJsonResponse(self, getContactListWithParameters, phoneNumber):
+        '''Pytest test case to validate the contact phoneNumbers inside the contactList '''
+        count = 0
+        for i in range(0, len(getContactListWithParameters['connections'])):
+
+            if phoneNumber in list(getContactListWithParameters['connections'][i]['phoneNumbers'][0].values()):
+                count = count + 1
+                break
+        if count > 0:
+            assert True
+        else:
+            assert False, phoneNumber + " does not exist in the contact list"
+
+    @pytest.mark.parametrize('email', Parameters_EmailAddress)
+    def test_verifyNamesInJsonResponse(self, getContactListWithParameters, email):
+        '''Pytest test case to validate the contact emailAddress inside the contactList '''
+        count = 0
+        for i in range(0, len(getContactListWithParameters['connections'])):
+
+            if email in list(getContactListWithParameters['connections'][i]['emailAddress'][0].values()):
+                count = count + 1
+                break
+        if count > 0:
+            assert True
+        else:
+            assert False, email + " does not exist in the contact list"
+
+
+class Test_verifyDataAccordingToParameters:
+    '''Test class to validate the data according to paramters passed in the test case. Parameters used is
+    personFields masks according to the google API docs'''
+
+    @pytest.mark.smoke
+    @pytest.mark.parametrize('params', Parameters)
+    def test_DataAccordingToParamenters(self, params):
+        # Parameterised test case executing multiple times according to number of test parameters
+        url = BaseUrl + "?personFields=" + params
+        response = requests.request("GET", url, headers=headers, data=payload)
+        jsonResponse = json.loads(response.text)
+        count = 0
+        if params in jsonResponse['connections'][0].keys():
+            count += 1
+        if count > 0:
+            assert True
+        else:
+            assert False, params + " does not exist in the contact list"
+
 
 
 
